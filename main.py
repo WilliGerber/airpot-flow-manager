4fdf16a4429bb59e9ac2cb6e90d346b40faad751
WilliGerber <49000944+WilliGerber@users.noreply.github.com>
2024-04-25 09:06:14 -0300

    create random planes ok

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# Abrir o arquivo de texto no modo de leitura
-
 from time import sleep
 from create_planes import Planes
 import random
+import threading
 
-
-
+planes = Planes()
 airplanes =   []
 
 def createPlanes():
     while True:
-        if airplanes.lenght < 10:
-            airplanes.append(Planes.createPlane())
+        if len(airplanes) < 10:
+            airplanes.append(planes.createPlane())
         sleep(random.randint(10,20))
         
 def increaseWaitingTime():
     while True:
         for airplane in airplanes:
-            airplane['waitingTime'] += 1
+            airplane['waiting_time'] += 1
         sleep(1)
         
 while True:
+    threading.Thread(target=createPlanes).start()
+    threading.Thread(target=increaseWaitingTime).start()
     
-    createPlanes()
-    increaseWaitingTime()
+    print(airplanes)
     
-    
+    sleep(1)
