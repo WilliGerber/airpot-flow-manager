6558b352e502e3c04ef0863b4823f5db0426d628
WilliGerber <49000944+WilliGerber@users.noreply.github.com>
2024-04-25 00:34:43 -0300

    create planes

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 # Abrir o arquivo de texto no modo de leitura
 
 from time import sleep
+from create_planes import Planes
+import random
 
 
-airplanesReady =   [
-                        {"code": 918390, "send_signal":"00:00:00 04-11-2024"},
-                        {"code": 293201, "send_signal":"01:00:00 04-11-2024"},
-                        {"code": 883821, "send_signal":"02:00:00 04-11-2024"}
-                    ]
 
+airplanes =   []
 
-for airplane in airplanesReady:
-    airplane['waitingTime'] = 0
-
+def createPlanes():
+    while True:
+        if airplanes.lenght < 10:
+            airplanes.append(Planes.createPlane())
+        sleep(random.randint(10,20))
+        
+def increaseWaitingTime():
+    while True:
+        for airplane in airplanes:
+            airplane['waitingTime'] += 1
+        sleep(1)
+        
 while True:
-    for airplane in airplanesReady:
-        airplane['waitingTime'] += 1
-        print(airplane['waitingTime'])
-    sleep(1)
-    print(airplanesReady)
+    
+    createPlanes()
+    increaseWaitingTime()
+    
+    
